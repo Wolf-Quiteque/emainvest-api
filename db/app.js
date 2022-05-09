ecce5d50640f49aac21f2156d38100439409887f
marcio <marcioqui3@gmail.com>
2022-05-09 13:44:45 +0100

    gotci

--- db/app.js
+++ db/app.js
@@ -1,47 +1,53 @@
-const express = require ("express")
-const dotenv = require("dotenv")
+const express = require("express");
+const dotenv = require("dotenv");
 
 const app = express();
-app.use (express.json());
+app.use(express.json());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
+  next();
+});
+
 dotenv.config();
 //routes
-const authRoute = require ("./routes/usuarios/auth");
-const empresaGetRoute = require ("./routes/empresas/empresaGet");
-const empresaPostRoute = require ("./routes/empresas/empresaPost");
-const empresaPutRoute = require ("./routes/empresas/empresaPut");
-const empresaDeleteRoute = require ("./routes/empresas/empresaDelete");
+const authRoute = require("./routes/usuarios/auth");
+const empresaGetRoute = require("./routes/empresas/empresaGet");
+const empresaPostRoute = require("./routes/empresas/empresaPost");
+const empresaPutRoute = require("./routes/empresas/empresaPut");
+const empresaDeleteRoute = require("./routes/empresas/empresaDelete");
 
-
-const usuarioPostRoute = require ("./routes/usuarios/usuarioPost");
+const usuarioPostRoute = require("./routes/usuarios/usuarioPost");
 const usuarioGetRoute = require("./routes/usuarios/usuarioGet");
-const usuarioDeleteRoute = require ("./routes/usuarios/usuarioDelete");
-const usuarioPutRoute = require ("./routes/usuarios/usuarioPut");
+const usuarioDeleteRoute = require("./routes/usuarios/usuarioDelete");
+const usuarioPutRoute = require("./routes/usuarios/usuarioPut");
 
-const relatorioGetRoute = require ("./routes/relatorio/relatorioGet");
-const relatorioPostRoute = require ("./routes/relatorio/relatorioPost");
+const relatorioGetRoute = require("./routes/relatorio/relatorioGet");
+const relatorioPostRoute = require("./routes/relatorio/relatorioPost");
 const relatorioPutRoute = require("./routes/relatorio/relatorioPut");
-const relatorioDeletetRoute = require ("./routes/relatorio/relatorioDelete");
+const relatorioDeletetRoute = require("./routes/relatorio/relatorioDelete");
 
-const negociacaoGetRoute = require ("./routes/webtrader/negociacao/negociacaoGet");
-const negociacaoPostRoute = require ("./routes/webtrader/negociacao/negociacaoPost");
+const negociacaoGetRoute = require("./routes/webtrader/negociacao/negociacaoGet");
+const negociacaoPostRoute = require("./routes/webtrader/negociacao/negociacaoPost");
 const negociacaoPutRoute = require("./routes/webtrader/negociacao/negociacaoPut");
-const negociacaoDeleteRoute = require ("./routes/webtrader/negociacao/negociacaoDelete");
+const negociacaoDeleteRoute = require("./routes/webtrader/negociacao/negociacaoDelete");
 
-const usuarioWbPostRoute = require ("./routes/webtrader/usuarios/usuarioPost");
+const usuarioWbPostRoute = require("./routes/webtrader/usuarios/usuarioPost");
 const usuarioWbGetRoute = require("./routes/webtrader/usuarios/usuarioGet");
-const usuarioWbDeleteRoute = require ("./routes/webtrader/usuarios/usuarioDelete");
-const usuarioWbPutRoute = require ("./routes/webtrader/usuarios/usuarioPut");
+const usuarioWbDeleteRoute = require("./routes/webtrader/usuarios/usuarioDelete");
+const usuarioWbPutRoute = require("./routes/webtrader/usuarios/usuarioPut");
 
-const ordemGetRoute = require ("./routes/webtrader/ordens/ordemGet");
-const ordemPostRoute = require ("./routes/webtrader/ordens/ordemPost");
+const ordemGetRoute = require("./routes/webtrader/ordens/ordemGet");
+const ordemPostRoute = require("./routes/webtrader/ordens/ordemPost");
 const ordemPutRoute = require("./routes/webtrader/ordens/ordemPut");
-const ordemDeleteRoute = require ("./routes/webtrader/ordens/ordemDelete");
+const ordemDeleteRoute = require("./routes/webtrader/ordens/ordemDelete");
 
-
-
-
-const port = 3000
-
+const port = 3000;
 
 //routes
 app.use("/api/db/usuarios/get", authRoute);
@@ -65,16 +71,14 @@ app.use("/api/db/webtrader/negociacao/Post", negociacaoPostRoute);
 app.use("/api/db/webtrader/negociacao/put", negociacaoPutRoute);
 app.use("/api/db/webtrader/negociacao/delete", negociacaoDeleteRoute);
 
-
 app.use("/api/db/webtrader/usuario/post", usuarioWbPostRoute);
 app.use("/api/db/webtrader/usuario/get", usuarioWbGetRoute);
 app.use("/api/db/webtrader/usuario/delete", usuarioWbDeleteRoute);
 app.use("/api/db/webtrader/usuario/put", usuarioWbPutRoute);
-
 
 app.use("/api/db/webtrader/ordem/post", ordemPostRoute);
 app.use("/api/db/webtrader/ordem/get", ordemGetRoute);
 app.use("/api/db/webtrader/ordem/delete", ordemDeleteRoute);
 app.use("/api/db/webtrader/ordem/put", ordemPutRoute);
 
-app.listen(port,()=>console.log("servidor esta ligado"))
+app.listen(port, () => console.log("servidor esta ligado"));
