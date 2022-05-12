1bb00748b42fcf4295f99d0788ecc7300b0e4a2d
marcio <marcioqui3@gmail.com>
2022-05-12 11:14:00 +0100

    oka

--- db/app.js
+++ db/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const clientPromise = require("./db/conexao");
+// const clientPromise = require("./db/conexao");
 
 const app = express();
 app.use(express.json());
